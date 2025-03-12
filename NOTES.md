a328f39b2d5990da828595c11ab6a5474fab8a1d
Tom <chovanectomas07@gmail.com>
2025-03-12 14:24:54 +0100

    added a simple test case

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+message starts with <STX> byte 0x02, then xml data, ends with <ETX> byte 0x03
+
+requester needs to wait for a reply to send another requests, does not include cancel reuqest
+
+transaction request conatins *TimeoutResponse* tag => timeout in seconds
+
+XML => UTF-8
+
+
--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+import socket
+
+port = 2605
+host = "127.0.0.1"
+
+
 def main() -> None:
-    print("Hewwo wowd :3")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("127.0.0.1", port))
+
+    print(f"listening on port: {port}")
+    server.listen(1)
+
+    conn, _ = server.accept()
+    with conn:
+        data = conn.recv(4096)
+        print(f"{data.decode()}")
 
 
 if __name__ == "__main__":
